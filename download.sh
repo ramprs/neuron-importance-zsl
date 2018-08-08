3a51ec27509f48fc5eeb2c4a0db9353afe68bbc7
ramprs <ram21@vt.edu>
2018-08-08 13:12:18 -0400

    includes download links for AWA2 and CUB images

--- download.sh
+++ download.sh
@@ -2,6 +2,17 @@
 wget https://filebox.ece.vt.edu/~ram21/Projects/neuron-importance-zsl/data.zip
 unzip data.zip
 
+# Download dataset images
+# AWA2
+cd data/AWA2/
+wget https://cvml.ist.ac.at/AwA2/AwA2-data.zip
+unzip AwA2-data.zip
+cd ../..
+# CUB
+cd data/CUB/
+wget http://www.vision.caltech.edu/visipedia-data/CUB-200/images.tgz
+tar -xvzf images.tgz
+
 # Download pretrained finetuned models and domain2alpha_checkpoints
 wget https://filebox.ece.vt.edu/~ram21/Projects/neuron-importance-zsl/ckpt.zip
 unzip ckpt.zip
